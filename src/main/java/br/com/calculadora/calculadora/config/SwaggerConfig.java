49c962cac6b6d5f577aa25a6d72bc6cd38e3cfff
Leandro Cunha <leandrocunha016@gmail.com>
2021-10-03 10:37:36 -0300

    Add server servlet context and spring jackson default

--- src/main/java/br/com/calculadora/calculadora/config/SwaggerConfig.java
+++ src/main/java/br/com/calculadora/calculadora/config/SwaggerConfig.java
@@ -9,7 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -22,6 +21,5 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
-
 }
 
